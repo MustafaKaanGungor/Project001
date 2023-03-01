9f395235c32fd467fcbb50ec46562d16230a82ba
Mustafa Kaan Güngör <kaan062004+github@gmail.com>
2023-03-01 10:03:02 +0300

    small changes

--- Project001/Assets/scriptler/gunControl.cs
+++ Project001/Assets/scriptler/gunControl.cs
@@ -9,6 +9,8 @@ public class gunControl : MonoBehaviour
 
     public GameObject gun;
 
+    public float rotLim = 50f;
+
 
      void Awake() {
 	
@@ -22,9 +24,28 @@ public class gunControl : MonoBehaviour
     // Update is called once per frame
     void Update() 
     {
-        if(PointerPosition.x > gun.transform.position.x && PointerPosition.y < gun.transform.position.y)
-        transform.right = (PointerPosition-(Vector2)transform.position).normalized;
+        LimitRot();
+
+        Quaternion targetRot = Quaternion.LookRotation(PointerPosition - transform.localRotation);
+
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRot, 10f * Time.deltaTime);
+        
+        //transform.right = (PointerPosition-(Vector2)transform.position).normalized;
+    
     }
+
+    private void LimitRot()
+    {
+        Vector3 playerEulerAngles = transform.localRotation.eulerAngles;
+
+        playerEulerAngles.z = (playerEulerAngles.z > 180) ? playerEulerAngles.z - 360 : playerEulerAngles.z;
+        playerEulerAngles.z = Mathf.Clamp(playerEulerAngles.z, -rotLim, rotLim);
+
+        Debug.Log(playerEulerAngles.z);
+
+        transform.localRotation = Quaternion.Euler(playerEulerAngles);
+    }
+    
 
     private void FixedUpdate() {
 
